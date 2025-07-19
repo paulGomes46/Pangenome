11a5e739d411f9f53987a59258400f367ae9f4b3
Paul Gomes <gomes.paul.46@gmail.com>
2025-07-19 14:31:21 +0800

    update

--- vg_tools_pipeline.sh
+++ vg_tools_pipeline.sh
@@ -15,38 +15,81 @@ BASENAME="graph_rna"
 echo "Building variation graph..."
 vg construct -r "$REF" -v "$VCF" -a -t $THREADS > "${BASENAME}.vg"
 
-# STEP 2: Add transcript paths from GFF
-echo "📚 Adding transcript paths..."
-vg rna \
-  -g /gss1/home/hzhao/paul/rna_seq/genome/pangenome/SWO.vg.gz \
-  -x /gss1/home/hzhao/paul/rna_seq/genome/pangenome/SWO.xg.gz \
-  -a /gss1/home/hzhao/paul/rna_seq/genome/hzao3.gff3 \
-  -t 8 \
-  > SWO_rna.vg
 
-
+# STEP 2: Add transcript paths from GFF   ----------- OK --------------
+# Note : the pangenome cannot be zipped
 vg rna \
 -p \
 --threads 8 \
 --transcripts /gss1/home/hzhao/paul/rna_seq/genome/hzao3.gff3 \
-/gss1/home/hzhao/paul/rna_seq/genome/pangenome/SWO.vg.gz \
-> spliced_SWO.pg
+/gss1/home/hzhao/paul/rna_seq/genome/pangenome/SWO.vg \
+> SWO_spliced.pg
 
 
-# STEP 3: Index the graph
-echo "🧩 Indexing the graph..."
-vg index -x "${BASENAME}.xg" "${BASENAME}_txp.vg"
-vg index -g "${BASENAME}.gcsa" -k 16 "${BASENAME}_txp.vg"
+# STEP 3: Index the graph     -------------- OK  ----------
+vg index -x SWO_spliced.xg -g SWO_spliced.gcsa -k 16 SWO_spliced.vg
 
-# STEP 4: Map RNA-seq reads using mpmap
+
+# STEP 4: Map RNA-seq reads using mpmap     ------------ In validation ------------
 echo "🎯 Mapping reads with vg mpmap..."
 vg mpmap \
-  -x "${BASENAME}.xg" \
-  -g "${BASENAME}.gcsa" \
-  -f "$READ1" \
-  -f "$READ2" \
-  -t $THREADS \
-  > "${BASENAME}.gamp"
+-x SWO_spliced.xg \
+-g SWO_spliced.gcsa \
+-f "$READ1" \
+-f "$READ2" \
+-t 8 \
+> SWO_spliced.gamp
+
+#in a Loop
+
+#Récuper la liste de tous les reads
+
+for file in /gss1/home/hzhao/paul/rna_seq/data/my_dataset/*trimmed*
+do
+        sample_name=$(basename  "$file" | cut -c1-6)
+        if [[ ! ${read_prefix[@]} =~ $sample_name ]]
+        then
+                read_prefix+=($sample_name)
+                echo $sample_name
+        fi
+done
+
+echo $read_prefix
+
+for element in ${read_prefix[@]}
+do
+        vg mpmap \
+        -x SWO_spliced.xg \
+        -g SWO_spliced.gcsa \
+        -f /gss1/home/hzhao/paul/rna_seq/data/my_dataset/${element}_1_trimmed_PE.fastq.gz \
+        -f /gss1/home/hzhao/paul/rna_seq/data/my_dataset/${element}_2_trimmed_PE.fastq.gz \
+        -t 8 \
+        > ${element}.gamp
+done
+
+
+#Align with Hisat2
+for element in ${read_prefix[@]}
+do
+        # be sure the name of the index is correct. I usually use genome_index_ + name of the genome
+        echo "alignment : $element"
+        hisat2 --dta -x $index \
+        -1 /gss1/home/hzhao/paul/rna_seq/data/my_dataset/${element}_1_trimmed_PE.fastq.gz \
+        -2 /gss1/home/hzhao/paul/rna_seq/data/my_dataset/${element}_2_trimmed_PE.fastq.gz \
+        --fr \
+        -S $alignment/${element}.sam \
+        --summary-file $alignment/log/${element}_alignment_summary.txt
+        samtools sort \
+        -o $alignment/${element}.bam \
+        $alignment/${element}.sam
+        #remove the .bam files to save space
+        rm $alignment/${element}.sam
+done
+
+
+
+
+
 
 # STEP 5: Convert GAMP to GAM (optional but needed for pack)
 echo "🔁 Converting .gamp to .gam..."
